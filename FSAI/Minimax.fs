9cee96615a6c6df588769f0e8a493e724920e535
Ahmad adnan Abdullah <70108548+abah19vo@users.noreply.github.com>
2022-01-04 12:38:07 +0100

    documented and cleaned code

--- FSAI/Minimax.fs
+++ FSAI/Minimax.fs
@@ -40,30 +40,32 @@ module Minimax =
 
 
     
-
+    // minimaxAlphaBeta function that returns scoer acording to the minimax alphabeta algorithm 
     let rec minimaxAlphaBeta (board:byte[,]) (depth:int) (a:int) (b:int) (tile:Byte) (isMaxPlayer:bool) (getWinner: byte[,] -> byte) (makeMove: byte[,] -> int * int-> byte -> byte[,]) (getValidMoves) (evaluation: byte[,] -> int) (otherTile: byte -> byte) =
         let validMovesResizeArray:ResizeArray<Tuple<int,int>> = getValidMoves board tile 
         let validMoves = validMovesResizeArray.ToArray() |> Array.toList
-        let emptyByte:byte = byte 0
-        let winnerExist =  (getWinner board) <> emptyByte
-        if(depth = 0 || winnerExist ) then evaluation board
+        let winnerExist =  (getWinner board) <> byte 0
+
+        if(depth = 0 || winnerExist || validMoves.Length < 1 ) then evaluation board
         else
             let childBoard:Byte[,] = Array2D.copy board
             let bestScore = getFakeBestScore isMaxPlayer
-            
+            // look into each valid move and returns the best score 
             let rec bestScoreResult a b childBoard bestScore moves = 
                 match moves with
                     |[] -> bestScore
                     |head::tail -> 
+                        // gets the nodes best score
                         let nodeScore = minimaxAlphaBeta childBoard (depth-1) a b (otherTile tile) (not isMaxPlayer) getWinner makeMove getValidMoves  evaluation  otherTile 
                         let newBoard = makeMove childBoard head tile 
                         let newA = newMaxA isMaxPlayer nodeScore a 
                         let newB =  newMinB isMaxPlayer nodeScore b
                         let newBestScore = minOrMax  isMaxPlayer bestScore nodeScore
+                        
                         if(shouldBreak isMaxPlayer newBestScore newA newB ) then newBestScore
                         else bestScoreResult newA newB newBoard newBestScore tail
 
             if( validMoves.Length > 0 ) then bestScoreResult a b childBoard bestScore validMoves
-            else minimaxAlphaBeta childBoard depth a b (otherTile tile) (not isMaxPlayer) getWinner makeMove getValidMoves  evaluation  otherTile 
+            else evaluation board
     
 
