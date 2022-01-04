b54a526a00df71eadb8ce6e8878f03edd436c22f
Umid0vic <57035358+Umid0vic@users.noreply.github.com>
2022-01-04 12:13:16 +0100

    Stödfunktioner för MiniMax

--- FSAI/Minimax.fs
+++ FSAI/Minimax.fs
@@ -1,9 +1,39 @@
 ﻿namespace FSAI
+open System
+
 
 module Minimax =
-    let printHello message = 
-        $"Hello, world {message}"
-
-    type Class1() = 
-        member this.X = "F#"
     
+    //Returns max or min between bestscore and nodeScore depending on isMaxPlayer value
+    let minOrMax isMaxPlayer bestScore nodeScore = 
+        if(isMaxPlayer) then max bestScore nodeScore
+        else min bestScore nodeScore
+    
+    //Checks AlphaBeta
+    let shouldBreak isMaxPlayer bestScore a b =
+        if(isMaxPlayer) then
+            let newA = max bestScore a
+            if(b <= newA) then true
+            else false
+        else
+            let newB = min bestScore b
+            if(newB <= a) then true
+            else false
+
+    //Ruturns lowest or highest int32 value
+    let getFakeBestScore isMaxPlayer =
+         if(isMaxPlayer) then Int32.MinValue
+         else Int32.MaxValue
+
+    let isEmpty (list) = 
+        match list with 
+        | [] -> true
+        | _ -> false
+
+    let newMaxA isMaxPlayer a bestScore = 
+        if(isMaxPlayer) then max bestScore a
+        else a
+    
+    let newMinB isMaxPlayer b bestScore = 
+         if( not isMaxPlayer) then min bestScore b
+         else b   
